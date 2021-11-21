0a1eb9097a754885dbccd88355eefb9cc836f49d
AymanMakram <aymanmmokhtar@gmail.com>
2021-11-21 16:02:51 +0200

    splash screen has been merged on development and created feature/home , also i created themedata.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
-
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'HomeScreen';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(children: [
+      Image(
+        image: AssetImage('assets/images/main_background.png'),
+        fit: BoxFit.cover,
+        width: double.infinity,
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text('إسلامي'),
+        ),
+      ),
+    ]);
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:myfirst_app_islami/HomeScreen.dart';
 
+import 'HomeScreen.dart';
+
 void main() {
-  runApp(MainApp());
+  runApp(MainApplication());
 }
 
-class MainApp extends StatelessWidget {
+class MyThemeData {
+  static final Color primarycolor = Color.fromARGB(255, 183, 147, 95);
+}
+
+class MainApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       title: 'Islami',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        primaryColor: MyThemeData.primarycolor,
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            )),
+      ),
       routes: {HomeScreen.routeName: (buildContext) => HomeScreen()},
       initialRoute: HomeScreen.routeName,
     );
